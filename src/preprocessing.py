4e64f9ec0004221bf856b682aa9eb444fe88b194
Ashwin Bobby <ashwin.bobbyv@gmail.com>
2026-09-11 11:04:57 +0530

    feat: preprocessor builder and data  cleaner functions added

---
+++ src/preprocessing.py
@@ -0,0 +1,34 @@
+import pandas as pd
+from sklearn.pipeline import Pipeline
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.compose import ColumnTransformer
+
+def load_data(path):
+    df = pd.read_csv(path)
+    df.drop(columns=["customerID"])
+    df["Churn"] = df["Churn"].map({"Yes": 1, "No":0})
+    return df
+
+def build_preprocessor(df):
+    X = df.drop(columns="Churn")
+    num_cols = X.selectdtypes(include="number").columns.tolist()
+    cat_cols = X.selectdtypes(include="object").columns.tolist()
+
+    num_pipe = Pipeline([
+        ("impute", SimpleImputer(strategy="median")),
+        ("scale", StandardScaler())
+    ])
+
+    cat_pipe = Pipeline([
+        ("impute", SimpleImputer(strategy="most_frequent")),
+        ("scale", OneHotEncoder(handle_unknown="ignore"))
+    ])
+
+    # ColumnTransformer takes the tuple ("transformerName", transformer, columns) for each subset you pass of the dataframe
+    preprocessor = ColumnTransformer([
+        ("num", num_pipe, num_cols),
+        ("cat", cat_pipe, cat_cols)
+    ])
+
+    return preprocessor, num_cols, cat_cols
